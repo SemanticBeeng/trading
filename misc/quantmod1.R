68836354a88f882600b7e66fa866e5e1a0b85e45
SemanticBeeng <nick@semanticbeeng.com>
2015-07-20 10:41:20 -0700

    study T&Q data aggregation

--- misc/quantmod1.R
+++ misc/quantmod1.R
@@ -7,45 +7,36 @@
 #)
 require(quantmod)
 require(FinancialInstrument)
-#require(PerformanceAnalytics)
-#require(xtsExtra)
-#require(rCharts)
-
-#setwd("/datascience/marketdata/storage")
-
-#setDefaults(getSymbols,src='RData')
-
-# get S&P 500 data from FRED (St. Louis Fed)
-#ORAN.PA <- na.omit( 
-#  getSymbols(
-#    "2013.07.01.ORAN.PA",
-#    src="RData",
-#    file.path="2015.01.02.ORAN.PA",
-#    dir = "ORAN.PA",
-#    extension = "RData",
-#    from = "2015-01-01",
-#    auto.assign = FALSE,
-#    verbose = TRUE
-#  )
-#)
 
 tmpenv <- new.env()
 
 symbol <- "ORAN.PA"
 from <- as.Date('2013-07-01')
 to <- from + 516
-tmpenv$ORAN.PA <- xts(1:517, as.Date(from:to))
+#tmpenv$ORAN.PA <- xts(1:517, as.Date(from:to))
 storageDir <- file.path("/datascience/marketdata/storage")
 
-getSymbols(
+#setSymbolLookup.FI(storage_method = "rda",
+#                   base_dir = file.path("/datascience/marketdata/storage"))
+
+tmpenv$ORAN.PA <- getSymbols(
     symbol,
-    from=from,
-    to=to,
-    src="FI",
+    from = from,
+    to = from + 5,
+    src = "FI",
     env = tmpenv,
-    dir=storageDir,
-    etension="RData",
+    dir = storageDir,
+    etension = "RData",
     auto.assign = FALSE,
     verbose = TRUE)
-# 
 
+has.Vo(tmpenv$ORAN.PA)
+has.Ask(tmpenv$ORAN.PA)
+has.Bid(tmpenv$ORAN.PA)
+has.HLC(tmpenv$ORAN.PA)
+tmpenv$ORAN.PA['2013-07-01 09']
+
+p <- periodicity(tmpenv$ORAN.PA)
+unclass(p)
+
+to.minutes5(tmpenv$ORAN.PA, "Price")
