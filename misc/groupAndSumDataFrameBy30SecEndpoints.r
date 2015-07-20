a69ecb834b219097bc70b2b501d589c384f5c304
SemanticBeeng <nick@semanticbeeng.com>
2015-07-20 02:59:08 -0700

    study T&Q data aggregation

--- misc/groupAndSumDataFrameBy30SecEndpoints.r
+++ misc/groupAndSumDataFrameBy30SecEndpoints.r
@@ -4,17 +4,18 @@
 r.xts <- read.table(textConnection("
 2011-05-05 09:29:31.929 0.5
 2011-05-05 09:29:59.929 0.5
-2011-05-05 09:30:04.929 0.3264757
-2011-05-05 09:30:14.907 0.0934498
-2011-05-05 09:30:19.917 0.8956367
-2011-05-05 09:30:35.114 1.6632110
-2011-05-05 09:30:45.193 1.1666715
-2011-05-05 09:31:12.417 0.2861861"))
+2011-05-05 09:30:04.929 0.1
+2011-05-05 09:30:14.907 0.2
+2011-05-05 09:30:19.917 0.3
+2011-05-05 09:30:35.114 1.1
+2011-05-05 09:30:45.193 1.2
+2011-05-05 09:31:12.417 0.4"))
 
-r.xts <- xts(r.xts$V3, as.POSIXct(paste(r.xts$V1,r.xts$V2)))
-r.xts
+r.xts <- xts(r.xts$V3, as.POSIXct(paste(r.xts$V1,r.xts$V2))); r.xts
 
-endpoints(r.xts, 'secs', k=30)
-period.sum(r.xts,endpoints(r.xts,'secs', k=30))
+ep <- endpoints(r.xts, 'secs', k=30); ep
 
-align.time(period.sum(r.xts,endpoints(r.xts,'secs',k=30)),30) 
+ps <- period.sum(r.xts, ep); ps
+
+align.time(ps, 30) 
+
