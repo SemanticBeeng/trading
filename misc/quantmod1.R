502ff9178fb00d3fd3346b70e85a3c34294867a8
SemanticBeeng <nick@semanticbeeng.com>
2015-07-24 02:40:39 -0700

    checkpoint:study T&Q data aggregation;AIRP.PA:merging;detect missing days

--- misc/quantmod1.R
+++ misc/quantmod1.R
@@ -12,22 +12,76 @@ require(lubridate)
 require(chron)
 
 tmpenv <- new.env()
-storageDir <- file.path("/datascience/marketdata/storage")
-# todo: set defaults
-#setSymbolLookup.FI(storage_method = "rda",
-#                   base_dir = file.path("/datascience/marketdata/storage"))
 
+symbol <- "AIRP.PA"
+
+# Set defaults
+#setSymbolLookup.FI(storage_method = "rda",
+#                   Symbols = c(symbol),
+#                   base_dir = file.path("/datascience/marketdata/storage"),
+#                   etension = "RData")
+
+# Filtering
 isChristmas <- function (x) {
-  #cat("isChristmas " + month(x) + day(x)  + " = " + (month(x) == 12 && (day(x) == 25 || day(x) == 26)) + "\n")
-  #cat("isChristmas " + x + "\n")
   return (month(x) == 12) && (day(x) == 25 || day(x) == 26)
 }
+
+########################################################
+# Produce the date ranges used for loading the data
+# Resources
+# http://stackoverflow.com/questions/11308367/error-in-my-code-object-of-type-closure-is-not-subsettable
+# http://www.win-vector.com/blog/2015/04/what-can-be-in-an-r-data-frame-column/
+# https://stat.ethz.ch/pipermail/r-help//2013-April/352482.html
+# http://stackoverflow.com/questions/11561856/add-new-row-to-dataframe
+# http://stackoverflow.com/questions/20689650/how-to-append-rows-to-an-r-data-frame
+dateRanges <- function() {
+
+  dummyDate <- as.POSIXct('1900-01-01', tz = "GMT")
+
+  df <- data.frame(y = 0:14, from = dummyDate, to = dummyDate, stringsAsFactors = FALSE)
   
-symbol <- "AIRP.PA"
+  # Change type after creation
+  # http://stackoverflow.com/a/4472094/4032515
+  #df[,1] <- as.POSIXct(df[,1])
+  #df[,2] <- as.POSIXct(df[,2])
+  
+  for(y in 0:14){
+  
+    from <- as.POSIXct(paste(2000 + y, '-01-01', sep = ""), tz = "GMT")
+    df$from[y+1] <- from
+    df$to[y+1] <- from + 1 * 365 * 86400
+  }
+
+  df$from[15] <- as.POSIXct(paste(2000 + 15, '-01-01', sep = ""), tz = "GMT")
+  df$to[15] <- as.POSIXct(paste(2000 + 15, '-07-08', sep = ""), tz = "GMT")
+  df
+}
+dateRanges()
+
+#df <- data.frame(from, to, stringsAsFactors = FALSE)
+df <- data.frame(start = character(16), end = character(16), stringsAsFactors = FALSE)
+
+df[,1] <- as.POSIXct(df[,1])
+df[,2] <- as.POSIXct(df[,2])
+
+y <- 0
+#f <- as.POSIXct(paste(2000 + y, '-01-01', sep = ""), tz = "GMT")
+f <- paste(2000 + y, '-01-01', sep = "")
+df$start[1] <- f
+df$end[1] <- f
+
+df$start[2] <- f
+df$end[1] <- f 
+
+x <- data.frame(date=as.POSIXct("2010-12-07 08:00:00"), value=NA)
+x$date[1] = as.POSIXct("2010-12-07 08:00:00")
+x$date[2] = as.POSIXct("2010-12-07 08:00:00")
+
 
 # Define the date range for data loading
 from <- as.POSIXct('2000-01-01', tz = "GMT")
 to <- from + 365 * 10 * 86400
+
 
 from <- as.POSIXct('2009-05-01', tz = "GMT")
 to <- as.POSIXct('2015-07-08', tz = "GMT")
@@ -40,9 +94,9 @@ result <- try(getSymbols(
             from = from,
             to = to,
             src = "FI",
-            env = tmpenv,
-            dir = storageDir,
+            dir = file.path("/datascience/marketdata/storage"),
             etension = "RData",
+            env = tmpenv,
             auto.assign = TRUE,
             verbose = TRUE))
 
@@ -98,14 +152,24 @@ head(symbolData, n = 50)
 
 # Source: http://www.quantmod.com/examples/data/
 
-#ps <- period.sum(tmpenv$ORAN.PA[, 1], endpoints(tmpenv$ORAN.PA, on = "minutes", k = 1)); 
-#ps
-#ts <- align.time(ps, 60)
-
+################################################################################
 # To OHLC
-ohlc <- to.period(symbolData[, 1:2], period = "minutes", k = 1440)
-colnames(ohlc) <- c("Open", "High", "Low", "Close", "Volume")
-ohlc <- align.time(ohlc, 60)
+toMinuteBars <- function(symbol, # : String
+                         kMinutes # : int
+                        ) {
+  #ps <- period.sum(tmpenv$ORAN.PA[, 1], endpoints(tmpenv$ORAN.PA, on = "minutes", k = 1)); 
+  #ps
+  #ts <- align.time(ps, 60)
+  
+  symbolData = get(symbol, tmpenv)
+
+  ohlc <- to.period(symbolData[, 1:2], period = "minutes", k = kMinutes)
+  colnames(ohlc) <- c("Open", "High", "Low", "Close", "Volume")
+
+  ohlc <- align.time(ohlc, 60)
+  
+  ohlc
+}
 
 # Zoom interactivelly: http://www.quantmod.com/documentation/zoomChart.html
 #chartSeries(x = window(ohlc, start = c(2000, 1), end = c(2000, 2)), name = symbol, TA='addVo()')
@@ -113,14 +177,18 @@ chartSeries(x = ohlc, name = symbol, TA= c(addVo()))
 zoomChart("2000-01::2000-05")
 zooom()
 
+################################################################################
 # googleVis charts
-ohlc.df =data.frame(date = as.Date(index(ohlc)), 
-                    open = as.numeric(ohlc$Open),
-                    close = as.numeric(ohlc$Close),
-                    high = as.numeric(ohlc$High),
-                    low = as.numeric(ohlc$Low))
+renderGoogleVisChart <- function(ohlc) {
 
-mplot = gvisCandlestickChart(ohlc.df, xvar = "date", low = "low", 
-                             open = "open", close = "close", high = "high",
-                             options = list(legend = 'none', width = 900, title = "Stock Price"))
-plot(mplot)
+  ohlc.df =data.frame(date = as.Date(index(ohlc)), 
+                      open = as.numeric(ohlc$Open),
+                      close = as.numeric(ohlc$Close),
+                      high = as.numeric(ohlc$High),
+                      low = as.numeric(ohlc$Low))
+  
+  mplot = gvisCandlestickChart(ohlc.df, xvar = "date", low = "low", 
+                               open = "open", close = "close", high = "high",
+                               options = list(legend = 'none', width = 900, title = "Stock Price"))
+  plot(mplot)
+}
