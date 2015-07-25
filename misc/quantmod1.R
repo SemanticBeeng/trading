2f72e97c6b17b7e4f7605bfaec60a19f2006714d
SemanticBeeng <nick@semanticbeeng.com>
2015-07-25 10:40:00 -0700

    checkpoint:study T&Q data aggregation;AIRP.PA;loads all data for a symbol and prints statistics including missing days;exclude Easter;works??

--- misc/quantmod1.R
+++ misc/quantmod1.R
@@ -85,7 +85,10 @@ symbols = data.frame(symbol = c("AEGN.AS", # all
 # https://www.euronext.com/en/holidays-and-hours
 # http://chronos-st.blogspot.ro/2008/03/easter-dates-from-2000-to-2020.html
 # http://stackoverflow.com/questions/6558921/r-boolean-operators-and
+# http://stackoverflow.com/questions/21888910/how-to-specify-names-of-columns-for-x-and-y-when-joining-in-dplyr
 # http://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
+# http://stackoverflow.com/questions/21618423/extract-a-dplyr-tbl-column-as-a-vector
+# http://stackoverflow.com/questions/27149306/dplyrselect-one-column-and-output-as-vector **
 Holidays = data.frame(year =  c(2000, 2000, 2001, 2001, 2002, 2002, 2003, 2003, 2004, 2004, 2005, 2005, 2006, 2006, 2007, 2007, 2008, 2008, 2009, 2009, 2010, 2010, 2011, 2011, 2012, 2012, 2013, 2013, 2014, 2014, 2015, 2015), 
                            month = c(   4,    4,    4,    4,    3,    4,    4,    4,    4,    4,    3,    3,    4,    4,    4,    4,    3,    3,    4,    4,    4,    4,    4,    4,    4,    4,    3,    4,    4,    4,    4,    4), 
                            day =   c(  21,   24,   13,   16,   29,    1,   18,   21,    9,   12,   25,   28,   14,   17,    6,    9,   21,   24,   10,   13,    2,    5,   22,   25,    6,    9,   29,    1,   18,   21,    3,    6)
@@ -96,7 +99,8 @@ isHoliday <- function (x) {
     xdf <- as.data.frame(x) %>% 
       mutate(year = year(x), month = month(x), day = as.numeric(day(x))) #%>%
       #select(-x)
-    isHoliday <- (left_join(xdf, Holidays, by = c("year" = "year", "month" = "month", "day" = "day")) %>% select(isHoliday))[,1]
+    isHoliday <- left_join(xdf, Holidays, by = c("year" = "year", "month" = "month", "day" = "day")) %>% .$isHoliday
+    #isHoliday <- (left_join(xdf, Holidays, by = c("year" = "year", "month" = "month", "day" = "day")) %>% select(isHoliday))[,1]
     #left_join(xdf, Holidays, by = c("year" = "year", "month" = "month", "day" = "day")) %>% collect %>% .[["Holidays"]]
     #anti_join(xdf, Holidays, by = c("year" = "year", "month" = "month", "day" = "day"))
     
